e8717370a2758e2df705e19d80746d18c8913fd1
GregWebDev <code@greggoodrem.co.uk>
2022-12-20 13:41:58 +0000

    Added adjust bag and remove from bag views and calc

--- bag/contexts.py
+++ bag/contexts.py
@@ -34,7 +34,7 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
@@ -49,7 +49,7 @@ def bag_contents(request):
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total
+        'grand_total': grand_total,
     }
 
     return context
